f967d7d4ed3a06256d874998d04be2b4897a4af8
Oleg1239 <130844415+Oleg1239@users.noreply.github.com>
2023-04-27 00:58:32 +0200

    Add files via upload
    Board table of the game Tic Tac Toe

---
+++ Cross nul 2.py
@@ -0,0 +1,14 @@
+board = list(range(1, 10))
+
+def draw_board():
+    global board  # added to access the global board variable
+    print('-----------')
+    for i in range(3):
+        print('|', board[0 + i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|')
+    print('-----------')
+
+
+draw_board()
+
+
+
